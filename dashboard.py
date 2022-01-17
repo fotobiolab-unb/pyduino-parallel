243ccc4774a3802e9ced7573ce00a158f210ef04
Icaro Costa <icarosadero@gmail.com>
2022-01-17 18:32:30 -0300

    Added  as column in log

--- dashboard.py
+++ dashboard.py
@@ -12,6 +12,7 @@ import yaml
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import plotly
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
@@ -68,7 +69,8 @@ external_stylesheets = [dbc.themes.CYBORG]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(
     html.Div([
-        html.H4('DASH'),
+        html.H1('DASH'),
+        html.P("\t".join(LOG_PATH)),
         dcc.Graph(id='live-update-graph',style={'height': y['subplot_height']*len(USE_COLS)}),
         dcc.Interval(
             id='interval-component',
