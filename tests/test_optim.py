9d48b38e3a756f8331786dbc7f28448a1e10b95c
Ícaro <icarosadero@gmail.com>
2024-03-10 00:01:16 -0300

    Added test against rosenbrock for nelder mead

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -1,4 +1,3 @@
-# Tests the gradient descent against a paraboloid of 4 dimensions.
 import sys
 import os
 import numpy as np
@@ -6,7 +5,7 @@ import numpy as np
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pyduino.optimization.nelder_mead import NelderMead
 
-def test_gradient_descent():
+def test_nelder_mead_parabola():
     optimizer = NelderMead(population_size=10, ranges=[(-10, 10)] * 4)
     class Oracle:
         def set(self, X):
@@ -21,6 +20,27 @@ def test_gradient_descent():
     optimizer.init_oracle()
 
     for i in range(100):
+        optimizer.step()
+    assert optimizer.view(optimizer.population).shape == (10, 4)
+    assert optimizer.view_g().shape == (10, 4)
+    assert optimizer.inverse_view(optimizer.view(optimizer.population)).shape == (10, 4)
+    assert np.isclose(optimizer.ask_oracle().min(), 0, atol=1e-4), f"Oracle: {optimizer.ask_oracle().min()}"
+
+def test_nelder_mead_rosenbrock():
+    optimizer = NelderMead(population_size=10, ranges=[(-10, 10)] * 4)
+    class Oracle:
+        def set(self, X):
+            self.X = X
+        def ask(self):
+            return ((1 - self.X[:, :-1])**2).sum(axis=1) + 100 * ((self.X[:, 1:] - self.X[:, :-1]**2)**2).sum(axis=1)
+    oracle = Oracle()
+    optimizer.ask_oracle = oracle.ask
+    optimizer.set_oracle = lambda X: oracle.set(X)
+
+    # Set initial population to oracle
+    optimizer.init_oracle()
+
+    for i in range(1000):
         optimizer.step()
     assert optimizer.view(optimizer.population).shape == (10, 4)
     assert optimizer.view_g().shape == (10, 4)
