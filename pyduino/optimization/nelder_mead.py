94a608afc53f44951c82346aa7127132a2178c90
Ícaro <icarosadero@gmail.com>
2024-03-29 02:03:41 -0300

    Debug Nelder Mead

--- pyduino/optimization/nelder_mead.py
+++ pyduino/optimization/nelder_mead.py
@@ -65,8 +65,8 @@ class NelderMead(Optimizer):
         """
         
         # Sort the population by the value of the oracle
-        self.y = self.ask_oracle(self.view_g())
-        idx = np.argsort(self.y)
+        y = self.ask_oracle(self.view_g())
+        idx = np.argsort(y)
         self.population = self.population[idx]
 
         # Compute the centroid of the population
@@ -81,7 +81,7 @@ class NelderMead(Optimizer):
 
         # If the reflected point is better than the second worst, but not better than the best, then expand
         
-        if y_reflected < self.y[-2] and y_reflected > self.y[0]:
+        if y_reflected < y[-2] and y_reflected > y[0]:
             expanded = centroid + (reflected - centroid)
             y_expanded = self.ask_oracle(self.view(expanded.reshape(1,-1)))
             if y_expanded < y_reflected:
@@ -89,7 +89,7 @@ class NelderMead(Optimizer):
             else:
                 self.population[-1] = reflected
         # If the reflected point is better than the best, then expand
-        elif y_reflected < self.y[0]:
+        elif y_reflected < y[0]:
             expanded = centroid + 2 * (reflected - centroid)
             y_expanded = self.ask_oracle(self.view(expanded.reshape(1,-1)))
             if y_expanded < y_reflected:
@@ -97,17 +97,18 @@ class NelderMead(Optimizer):
             else:
                 self.population[-1] = reflected
         # If the reflected point is worse than the second worst, then contract
-        elif y_reflected > self.y[-2]:
+        elif y_reflected > y[-2]:
             contracted = centroid + 0.5 * (self.population[-1] - centroid)
             y_contracted = self.ask_oracle(self.view(contracted.reshape(1,-1)))
-            if y_contracted < self.y[-1]:
+            if y_contracted < y[-1]:
                 self.population[-1] = contracted
             else:
                 for i in range(1, len(self.population)):
                     self.population[i] = 0.5 * (self.population[i] + self.population[0])
         # If the reflected point is worse than the worst, then shrink
-        elif y_reflected > self.y[-1]:
+        elif y_reflected > y[-1]:
             for i in range(1, len(self.population)):
                 self.population[i] = 0.5 * (self.population[i] + self.population[0])
 
+        self.y = y.copy()
         return self.view_g()
